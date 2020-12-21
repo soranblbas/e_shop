72e38a78b9dbecebd2311b9d954ed41e30cea076
soranblbas <76163231+soranblbas@users.noreply.github.com>
2020-12-21 22:20:48 +0300

    First_app

--- cosmotic/models.py
+++ cosmotic/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=14)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    image = models.ImageField(upload_to="static\cosmotic\img")
+
+    def __str__(self):
+        return self.name
--- cosmotic/views.py
+++ cosmotic/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
+
+from .models import *
 
 
 def home(request):
+
     return render(request, 'cosmotic/base.html')
 
+
+def products(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'cosmotic/products.html', context)
